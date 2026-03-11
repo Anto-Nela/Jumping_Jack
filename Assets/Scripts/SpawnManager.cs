2513f1fcc03b787f92e852bc64cd1102838e7d63
Antonela Hebipari <antonelahebipari@gmail.com>
2026-03-11 17:00:48 +0100

    Adding all the files plus assets

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using Vector3 = UnityEngine.Vector3;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(25,0,0);
+    private float startDelay = 2f;
+    private float repeatRate = 2f;
+    private PlayerController playerControllerScript;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnObstacle()
+    {
+        if (playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+    }
+}
